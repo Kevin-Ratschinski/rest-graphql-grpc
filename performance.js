9e01d1bc42b06de0dafd5963860214415141a24f
Kevin Ratschinski <k.ratschinski@icloud.com>
2022-05-17 12:43:50 +0200

    add grpc test

--- performance.js
+++ performance.js
@@ -1,8 +1,9 @@
 'use strict';
-const { performance, PerformanceObserver } = require('perf_hooks');
 const axios = require('axios');
+const grpcClient = require('./grpc/client/client');
+const { performance, PerformanceObserver } = require('perf_hooks');
 
-const REQUEST_COUNT = process.env.REQUEST_COUNT || 1000;
+const REQUEST_COUNT = 1000;
 
 const observer = new PerformanceObserver((list) => {
   list.getEntries().forEach((entry) => {
@@ -24,14 +25,38 @@ const performanceRest = async (endpoint) => {
   performance.mark('rest-end');
 
   performance.measure(
-    `http://localhost:5001${endpoint}`,
+    `REST http://localhost:5001${endpoint}`,
     'rest-start',
     'rest-end'
   );
+};
+
+const getAllUsers = (...args) => {
+  return new Promise((resolve, reject) => {
+    grpcClient.getAllUsers(...args, (error, users) => {
+      if (error) return reject(error);
+      resolve(users);
+    });
+  });
+};
+
+const performanceGRPC = async () => {
+  try {
+    performance.mark('grpc-start');
+    for (let i = 1; i <= REQUEST_COUNT; i++) {
+      await getAllUsers({});
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
+  performance.mark('grpc-end');
+
+  performance.measure(`gRPC http://localhost:5000`, 'grpc-start', 'grpc-end');
 };
 
 (async () => {
   await performanceRest('/users');
   await performanceRest('/user/1');
   await performanceRest('/messages');
+  await performanceGRPC();
 })();
